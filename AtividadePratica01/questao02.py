b91865706a05284b09226a793c82cc4a1c4ccc54
Augusto Mota <117319071+augustomotas@users.noreply.github.com>
2025-06-24 08:53:32 -0300

    Add files via upload

---
+++ AtividadePratica01/questao02.py
@@ -0,0 +1,8 @@
+# Calculadora de Soma
+
+numero1 = 12
+numero2 = 14
+
+resultado_soma = numero1 + numero2
+
+print(f"A soma de {numero1} e {numero2} é igual a {resultado_soma}")
---
+++ AtividadePratica01/questao04.py
@@ -0,0 +1,14 @@
+# Calculadora de Preço Total
+
+nome_produto = "Cadeira Infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = preco_unitario * quantidade
+
+print("--- Resumo da Compra ---")
+print(f"Produto: {nome_produto}")
+print(f"Preço por unidade: R$ {preco_unitario:.2f}")
+print(f"Quantidade: {quantidade}")
+print("------------------------")
+print(f"Valor Total a Pagar: R$ {preco_total:.2f}")
